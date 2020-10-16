f79746322bd0e70d9eaf26167fe797968e86500d
Óscar Paz <19271035+OscarPaz@users.noreply.github.com>
2020-10-16 22:12:41 +0200

    Edited build config file

--- config.mk
+++ config.mk
@@ -2,8 +2,9 @@
 VERSION = 5.0
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+LOCAL = ${HOME}/.local/share
+PREFIX = ${LOCAL}/dmenu
+MANPREFIX = ${LOCAL}/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
